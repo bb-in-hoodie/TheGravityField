4391cfc5d1af4cce8f94b03f8061aeea33a532b2
ParkSungOh <pso999@naver.com>
2016-05-26 00:22:46 +0900

    Ball Prefab Added / Test Scene Added
    - Ball prefab is added (w/o any function)
    - First test scene has been added
    - CameraManager is added (Main camera will follow the ball)

---
+++ The Gravity Field/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+    Vector3 gap;
+    Transform ballTrans;
+	// Use this for initialization
+	void Start () {
+        ballTrans = GameObject.Find("Ball").transform;
+        gap = transform.position - ballTrans.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = ballTrans.position + gap;
+	}
+}
