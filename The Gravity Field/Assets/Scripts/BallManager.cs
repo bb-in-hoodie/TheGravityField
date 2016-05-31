191a59a7258c7f89615ddb4a8bd318ba377e2892
yunjey <yunjey47@naver.com>
2016-06-01 00:28:26 +0900

    Creating Ball Manger and Change Color
    - BallManager.cs are added in scripts folder (scripts/BallManager.cs).
    - Ball object is now movable. If you press left or right key, it moves. If you press 'space bar' key, it jumps.
    - Ball object is changed to red color .

---
+++ The Gravity Field/Assets/Scripts/BallManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallManager : MonoBehaviour {
+	
+	public float speed;
+	public float jumpSpeed;
+    private Rigidbody rb;
+	
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+
+        // move left or right
+        rb.AddForce (movement * speed);
+
+        // jump when you press space bar button
+        if (Input.GetButton("Jump")) 
+     		rb.AddForce(Vector3.up * jumpSpeed);
+	}
+	
+
+	
+
+}
