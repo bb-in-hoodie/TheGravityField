3c547221a3dd63928a0428aa73a3f489f9957df1
blackbindy <pso999@naver.com>
2016-06-22 02:50:23 +0900

    Breachable Floor Added
    - BreachableFloor is added

---
+++ The Gravity Field/Assets/Scripts/BFManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BFManager : MonoBehaviour {
+    float alpha = 1.0f;
+    bool doDestroy = false;
+
+    void FixedUpdate()
+    {
+        if(doDestroy)
+        {
+            if (alpha >= 0)
+            {
+                alpha -= 0.01f;
+                Color oldColor = GetComponent<Renderer>().material.color;
+                GetComponent<Renderer>().material.color = new Color(oldColor.r, oldColor.g, oldColor.b, alpha);
+            }
+            else
+                Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DestroyBF()
+    {
+        transform.parent = null;
+        yield return new WaitForSeconds(3.0f);
+        doDestroy = true;
+    }
+}
---
+++ The Gravity Field/Assets/Scripts/BreachableFloorManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreachableFloorManager : MonoBehaviour {
+    public float velThres = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "BALL")
+            print(col.GetComponent<Rigidbody>().velocity);
+
+        if(col.tag == "BALL" && col.GetComponent<Rigidbody>().velocity.y < -velThres)
+        {
+            Destroy(transform.FindChild("Prop").gameObject);
+            foreach (BFManager bfm in transform.GetComponentsInChildren<BFManager>())
+            {
+                if (bfm.gameObject.name == "BF")
+                    bfm.SendMessage("DestroyBF");
+            }
+        }
+    }    
+}
