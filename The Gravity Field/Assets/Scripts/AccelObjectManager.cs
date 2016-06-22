75bc3ff85295b3ee48209f7c21b49ebfc38a059b
ParkSungOh <pso999@naver.com>
2016-06-22 18:19:34 +0900

    Accelerometer Recognition Added

---
+++ The Gravity Field/Assets/Scripts/AccelObjectManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AccelObjectManager : MonoBehaviour, AccelInteractive {
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void OnAccelActive()
+    {
+        rb.useGravity = true;
+    }
+}
