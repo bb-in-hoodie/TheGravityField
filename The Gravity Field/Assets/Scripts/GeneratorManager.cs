cf9342be90325960dc3631618d0ad815aebf3448
Inho Kim <Inho Kim>
2016-06-22 21:31:02 +0900

    6th: generator and switch added
    - If player enters the switch, the generator falls the falling object.
    - If player exits from the switch, the generator stops falling the falling object

---
+++ The Gravity Field/Assets/Scripts/GeneratorManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GeneratorManager : MonoBehaviour
+{
+    public GameObject targetObject;
+    public float waitTime = 3.0f;
+    //public bool shootOnFBPressed = false;
+
+    Transform targetPoint;
+    public bool shouldStop = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetPoint = transform.FindChild("TargetPoint");
+
+        if (targetObject != null)
+            StartCoroutine("CreateObject");
+    }
+   
+    IEnumerator CreateObject()
+    {
+        while(shouldStop == false)
+        {
+            GameObject newObj = (GameObject) Instantiate(targetObject, targetPoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(waitTime);
+            if (shouldStop == false)
+                Destroy(newObj);
+        }
+    }
+/*
+    public void OnFloorButtonPressed()
+    {
+        if (shootOnFBPressed)  // If shootOnFBPressed is true, dispenser shoot the target only when the floor button is pressed
+        {
+            shouldStop = false;
+            StartCoroutine("CreateObject");
+        }
+        else  // If shootOnFBPressed is false and FB is pressed, It means that dispenser doesn't have to shoot the target anymore
+        {
+            shouldStop = true;
+        }
+    }
+
+    public void OnFloorButtonReleased()
+    {
+        if (shootOnFBPressed)
+            shouldStop = true;
+    }
+    */
+}
---
+++ The Gravity Field/Assets/Scripts/SwitchManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchManager : MonoBehaviour {
+
+	// Use this for initialization
+    GeneratorManager gen;
+
+    // Use this for initialization
+    void Start()
+    {
+        gen = GameObject.Find("Generator").GetComponent<GeneratorManager>();
+    }
+	
+	void OnCollisionEnter(Collision col)
+    {
+        foreach (ContactPoint cp in col.contacts)
+        {
+            if (cp.otherCollider.tag == "BALL" || cp.otherCollider.tag == "FALLING" || cp.otherCollider.tag == "OBSTACLE" )
+                gen.shouldStop = false;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.collider.tag == "BALL" || col.collider.tag == "FALLING" || col.collider.tag == "OBSTACLE")
+            gen.shouldStop = true;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+    	if(other.tag == "BALL")
+    		gen.shouldStop = false;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+    	if(other.tag == "BALL")
+    		gen.shouldStop = true;
+    }
+}
