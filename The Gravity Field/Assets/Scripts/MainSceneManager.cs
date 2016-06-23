66c3cb977ac897dfe651ff01d1531b7e278860ca
ParkSungOh <pso999@naver.com>
2016-06-23 21:03:41 +0900

    MainScene Created

---
+++ The Gravity Field/Assets/Scripts/MainSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainSceneManager : MonoBehaviour, IPointerClickHandler
+{
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (gameObject.name == "Level1")
+            SceneManager.LoadScene("Level01");
+        if (gameObject.name == "Level2")
+            SceneManager.LoadScene("Level02");
+        if (gameObject.name == "Level3")
+            SceneManager.LoadScene("Level03");
+        if (gameObject.name == "LevelMain")
+            SceneManager.LoadScene("Main");
+    }
+}
